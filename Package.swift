e694ae6c0b9ed4868feb8e9324a4f8b1571b94e2
Ahmed Onawale <ahmedonawale@gmail.com>
2017-04-01 19:54:16 +0100

    Include jwt dependency

--- Package.swift
+++ Package.swift
@@ -3,14 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Passwordless",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
-    ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url:"https://github.com/vapor/jwt.git", majorVersion: 0, minor: 6)
     ]
 )
 
